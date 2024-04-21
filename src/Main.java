c069753d7297c7af3adc3922bb144fcb435e8452
Nurbolagybetov <154108956+Nurbolagybetov@users.noreply.github.com>
2024-04-22 04:13:16 +0500

    f

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Testing MyArrayList:");
+        MyList<Integer> arrayList = new MyArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        System.out.println("Size of list: " + arrayList.size());
+        System.out.println("Element at index 1: " + arrayList.get(1));
+        arrayList.remove(0);
+        System.out.println("Size of list after removal: " + arrayList.size());
+        arrayList.clear();
+        System.out.println("Size of list after clear: " + arrayList.size());
+        System.out.println("Is list empty: " + arrayList.isEmpty());
+
+        System.out.println("\nTesting MyLinkedList:");
+        MyList<String> linkedList = new MyLinkedList<>();
+        linkedList.add("A");
+        linkedList.add("B");
+        linkedList.add("C");
+        System.out.println("Size of list: " + linkedList.size());
+        System.out.println("Element at index 1: " + linkedList.get(1));
+        linkedList.remove(0);
+        System.out.println("Size of list after removal: " + linkedList.size());
+        linkedList.clear();
+        System.out.println("Size of list after clear: " + linkedList.size());
+        System.out.println("Is list empty: " + linkedList.isEmpty());
+    }
+}
---
+++ src/MyArrayList.java
@@ -0,0 +1,166 @@
+import java.util.Arrays;
+
+public class MyArrayList<T> implements MyList<T> {
+    private static final int def = 10;
+    private Object[] elements;
+    private int size;
+
+    public MyArrayList() {
+        elements = new Object[def];
+        size = 0;
+    }
+
+    @Override
+    public void add(T item) {
+        ensureCapacity();
+        elements[size++] = item;
+    }
+
+    @Override
+    public void set(int index, T item) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        elements[index] = item;
+    }
+
+    @Override
+    public void add(int index, T item) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        ensureCapacity();
+        System.arraycopy(elements, index, elements, index + 1, size - index);
+        elements[index] = item;
+        size++;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        add(0, item);
+    }
+
+    @Override
+    public void addLast(T item) {
+        add(size, item);
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        return (T) elements[index];
+    }
+
+    @Override
+    public T getFirst() {
+        if (isEmpty()) {
+            throw new IllegalStateException("List is empty");
+        }
+        return (T) elements[0];
+    }
+
+    @Override
+    public T getLast() {
+        if (isEmpty()) {
+            throw new IllegalStateException("List is empty");
+        }
+        return (T) elements[size - 1];
+    }
+
+    @Override
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        size--;
+    }
+
+    @Override
+    public void removeFirst() {
+        remove(0);
+    }
+
+    @Override
+    public void removeLast() {
+        remove(size - 1);
+    }
+
+    @Override
+    public void sort() {
+        Arrays.sort(elements, 0, size);
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        for (int i = 0; i < size; i++) {
+            if (object.equals(elements[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (object.equals(elements[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean exists(Object object) {
+        return indexOf(object) != -1;
+    }
+
+    @Override
+    public void clear() {
+        Arrays.fill(elements, null);
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public java.util.Iterator<T> iterator() {
+        return new Iterator<>();
+    }
+
+    private void ensureCapacity() {
+        if (size == elements.length) {
+            int newCapacity = elements.length * 2;
+            elements = Arrays.copyOf(elements, newCapacity);
+        }
+    }
+
+    private class Iterator<T> implements java.util.Iterator<T> {
+        private int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return index < size;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new java.util.NoSuchElementException();
+            }
+            return (T) elements[index++];
+        }
+    }
+}
+
